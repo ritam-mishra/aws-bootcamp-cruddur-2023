faed1a814c557c63184d6407b0ed564e55a13aef
ritam-mishra <ritammishra2002@gmail.com>
2023-08-05 17:59:53 +0000

    make profile image work, lambda code update

--- aws/lambdas/cruddur-upload-avatar/function.rb
+++ aws/lambdas/cruddur-upload-avatar/function.rb
@@ -1,48 +1,68 @@
 require 'aws-sdk-s3'
 require 'json'
-require 'jwt'
+
 
 def handler(event:, context:)
-  puts event
-  # return cors headers for preflight check
-  if event['routeKey'] == "OPTIONS /{proxy+}"
-    puts({step: 'preflight', message: 'preflight CORS check'}.to_json)
+
+
+  allowed_origins = [
+  
+    "https://3000-ritammishra-awsbootcamp-pd2rwrjntmd.ws-us102.gitpod.io",
+
+  ]
+
+  origin = event['headers']['origin'] || event['headers']['Origin']
+
+  if event["routeKey"] == "OPTIONS /{proxy+}"
+
+    puts({step:'preflight', message:"preflight CORS check"}.to_json)
+
     { 
       headers: {
         "Access-Control-Allow-Headers": "*, Authorization",
-        "Access-Control-Allow-Origin": "https://ritammishra-awsbootcamp-zgsalum6yp1.ws-us102.gitpod.io/",
+        "Access-Control-Allow-Origin": allowed_origins.include?(origin) ? origin : "",
         "Access-Control-Allow-Methods": "OPTIONS,GET,POST"
       },
-      statusCode: 200
+      statusCode: 200, 
     }
+    
   else
-    token = event['headers']['authorization'].split(' ')[1]
-    puts({step: 'presignedurl', access_token: token}.to_json)
-
+    puts 'printing the events==='
+    puts event
+    puts 'printing the origin===='
+    puts origin
     body_hash = JSON.parse(event["body"])
     extension = body_hash["extension"]
+    
+    cognito_user_id = event["requestContext"]["authorizer"]["lambda"]["sub"]
 
-    decoded_token = JWT.decode token, nil, false
-    cognito_user_uuid = decoded_token[0]['sub']
+    puts({step:'presign url', sub_value: cognito_user_id}.to_json)
+    
 
     s3 = Aws::S3::Resource.new
     bucket_name = ENV["UPLOADS_BUCKET_NAME"]
-    object_key = "#{cognito_user_uuid}.#{extension}"
+    object_key = "#{cognito_user_id}.#{extension}"
 
     puts({object_key: object_key}.to_json)
-
+  
     obj = s3.bucket(bucket_name).object(object_key)
     url = obj.presigned_url(:put, expires_in: 60 * 5)
     url # this is the data that will be returned
+
+  
     body = {url: url}.to_json
+    
     { 
       headers: {
         "Access-Control-Allow-Headers": "*, Authorization",
-        "Access-Control-Allow-Origin": "https://ritammishra-awsbootcamp-zgsalum6yp1.ws-us102.gitpod.io/",
+        "Access-Control-Allow-Origin": allowed_origins.include?(origin) ? origin : "",
         "Access-Control-Allow-Methods": "OPTIONS,GET,POST"
       },
       statusCode: 200, 
       body: body 
+      
     }
-  end # if 
-end # def handler
+    
+  end
+
+end
--- aws/lambdas/lambda-authorizer/index.js
+++ aws/lambdas/lambda-authorizer/index.js
@@ -3,28 +3,36 @@ const { CognitoJwtVerifier } = require("aws-jwt-verify");
 //const { assertStringEquals } = require("aws-jwt-verify/assert");
 
 const jwtVerifier = CognitoJwtVerifier.create({
-  userPoolId: process.env.USER_POOL_ID,
-  tokenUse: "access",
-  clientId: process.env.CLIENT_ID//,
-  //customJwtCheck: ({ payload }) => {
-  //  assertStringEquals("e-mail", payload["email"], process.env.USER_EMAIL);
-  //},
+    userPoolId: process.env.USER_POOL_ID,
+    tokenUse: "access",
+    clientId: process.env.CLIENT_ID//,
+    //customJwtCheck: ({ payload }) => {
+    //  assertStringEquals("e-mail", payload["email"], process.env.USER_EMAIL);
+    //},
 });
 
 exports.handler = async (event) => {
-  console.log("request:", JSON.stringify(event, undefined, 2));
 
-  const jwt = event.headers.authorization;
-  try {
-    const payload = await jwtVerifier.verify(jwt);
-    console.log("Access allowed. JWT payload:", payload);
-  } catch (err) {
-    console.error("Access forbidden:", err);
-    return {
-      isAuthorized: false,
-    };
-  }
-  return {
-    isAuthorized: true,
-  };
+
+    console.log("request:", JSON.stringify(event, undefined, 2));
+
+    const auth = event.headers.authorization;
+    const jwt = auth.split(" ")[1]
+
+    try {
+        const payload = await jwtVerifier.verify(jwt);
+        console.log("Access allowed. JWT payload:", payload);
+        return {
+            isAuthorized: true,
+            "context": {
+                "sub": payload.sub
+            },
+        };
+    } catch (err) {
+        console.error("Access forbidden:", err);
+        return {
+            isAuthorized: false,
+        };
+    }
+
 };
